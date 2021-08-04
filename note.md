379d4913344001077d10249c9c38daf808a9c767
tanut-surface <tanat44@gmail.com>
2021-08-04 10:12:33 +0700

    modify gallery written in jsx

---
+++ note.md
@@ -0,0 +1,3 @@
+To run development of React locally (using in-browser babel transformer), serve the html/js file on python server.
+
+    python3 -m http.server
---
+++ page/gallery.js
@@ -0,0 +1,52 @@
+
+class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [
+
+                {
+                    link: 'https://i.pinimg.com/originals/67/fa/40/67fa4031e7355e21b362ae62ed572356.jpg',
+                    description: 'Porsche 911'
+                },
+                {
+                    link: 'https://i.pinimg.com/originals/10/95/eb/1095eb0264ace768bf0ffc45b961bc1c.jpg',
+                    description: 'Volkswagen Golf GTI mk1'
+                },
+
+                {
+                    link: 'https://i.pinimg.com/originals/41/c0/b5/41c0b53264554f8ac2e2214e6c0e7c96.jpg',
+                    description: 'Ferrari 275GTB'
+                },
+                {
+                    link: 'https://i.pinimg.com/originals/5a/b2/f4/5ab2f478b99d197881d653f17de3adaf.jpg',
+                    description: 'Saint Louis Mansion Drizzling'
+                },
+            ]
+        };
+    }
+
+    answer(correct) {
+        this.setState({
+            correct
+        });
+    }
+
+    render() {
+        const state = this.state
+        return (
+            state.images.map( i => 
+                <div className="gallery">
+                    <a target="_blank" href={i.link}>
+                        <img src={i.link} alt={i.description}/>
+                    </a>
+                    <div className="desc">{i.description}</div>
+                </div>
+            )
+        );
+    }
+}
+ReactDOM.render(
+    <Gallery />,
+    document.getElementById('galleryContainer')
+);
