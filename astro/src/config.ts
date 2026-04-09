0b6e79c908e7239649c0f4c03eb7e50670b833c2
Tanut-M2 <tanat44@gmail.com>
2026-04-09 21:25:46 +0200

    add astro

---
+++ astro/src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://tanat44.com/", // replace this with your deployed domain
+  author: "Tanut Treratanakulwong",
+  profile: "https://tanat44.com/",
+  desc: "tanat44's blog, projects, books, cv",
+  title: "tanat44",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 10,
+  postPerPage: 10,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
